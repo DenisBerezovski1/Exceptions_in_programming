be06e305e31109f866d95032ff17eb24f104f95b
DenisBerezovski1 <119109483+DenisBerezovski1@users.noreply.github.com>
2023-09-02 06:42:17 +0300

    ДЗ у уроку 2

---
+++ Main.java
@@ -0,0 +1,27 @@
+import task.method1;
+import task.tasks1;
+import task.tasks3;
+import task.method3;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//  задача 1:
+        method1 m1 = new method1();
+        m1.method();
+
+//  задача 2.1:
+        new tasks1();
+
+//  задача 2.2:
+        new tasks3();
+
+//  задача 3:
+        method3 m3 = new method3();
+        m3.method();
+
+
+    }
+}
